346fbce28d6f914851d22d61b819d24f12961366
Stéphane Klein <contact@stephane-klein.info>
2014-07-01 11:59:29 +0200

    Update with last pip, setuptools, virtualenv version

--- bootstrap.py
+++ bootstrap.py
@@ -24,13 +24,13 @@ def local_adjust_options(options, args):
     tmp_dir = mkdtemp()
     print('download setuptools...')
     f = open(path_join(tmp_dir, 'setuptools-latest.tar.gz'), 'w')
-    f.write(urlopen('https://pypi.python.org/packages/source/s/setuptools/setuptools-1.1.1.tar.gz#md5=c8d19510c03b0e2e01880c0d8f080083').read())  # NOQA
+    f.write(urlopen('https://pypi.python.org/packages/source/s/setuptools/setuptools-5.3.tar.gz#md5=c8d19510c03b0e2e01880c0d8f080083').read())  # NOQA
     f.close()
     print('setuptools downloaded')
 
     print('download pip...')
     f = open(path_join(tmp_dir, 'pip-latest.tar.gz'), 'w')
-    f.write(urlopen('https://pypi.python.org/packages/source/p/pip/pip-1.4.1.tar.gz#md5=6afbb46aeb48abac658d4df742bff714').read())  # NOQA
+    f.write(urlopen('https://pypi.python.org/packages/source/p/pip/pip-1.5.6.tar.gz#md5=6afbb46aeb48abac658d4df742bff714').read())  # NOQA
     f.close()
     print('pip downloaded')
 
